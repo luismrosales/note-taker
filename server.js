81f12383c4b597fc22d294d0a351325d090880b1
Luis Rosales <luismrosales2@gmail.com>
2022-01-14 13:21:54 -0500

    refactoring code to make it cleaner

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
 const express = require("express");
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { db } = require("./Develop/db/db.json");
 const fs = require("fs");
 const req = require("express/lib/request");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 fs.readFile("notes.html", (err, data) => {
   console.log(data);
 });
 
-app.get("/api/notes", (req, res) => {
-  let results = db;
-
-  res.json(db);
-});
-
-app.post("/api/notes"),
-  (req, res) => {
-    let newNote = db;
-    res;
-  };
-
-app.delete("api/notes/:id"), (req, res) => {};
-
-app.listen(3001, () => {
-  console.log(`API server now on port 3001`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
 });
